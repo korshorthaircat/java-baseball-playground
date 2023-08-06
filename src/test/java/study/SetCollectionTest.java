03d2c1eb4769ff048414abd7110fce3ac9d62af4
KoreanShortHair <korshorthaircat@gmail.com>
2023-08-06 19:00:36 +0900

    학습 테스트(2)

--- src/test/java/study/SetCollectionTest.java
+++ src/test/java/study/SetCollectionTest.java
@@ -1,9 +1,16 @@
 package study;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Set Collection에 대한 학습 테스트
@@ -24,6 +31,10 @@ public class SetCollectionTest {
     요구사항 1
     Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트를 구현한다.
      */
+    @Test
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
 
     /*
     요구사항 2
@@ -31,11 +42,22 @@ public class SetCollectionTest {
     구현하고 보니 다음과 같이 중복 코드가 계속해서 발생한다.
     JUnit의 ParameterizedTest를 활용해 중복 코드를 제거해 본다.
      */
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(Integer input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
 
     /*
     요구사항 3
     요구사항 2는 contains 메소드 결과 값이 true인 경우만 테스트 가능하다. 입력 값에 따라 결과 값이 다른 경우에 대한 테스트도 가능하도록 구현한다.
     예를 들어 1, 2, 3 값은 contains 메소드 실행결과 true, 4, 5 값을 넣으면 false 가 반환되는 테스트를 하나의 Test Case로 구현한다.
      */
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void containsYN(Integer input, boolean expected) {
+        boolean containsYN = numbers.contains(input);
+        Assertions.assertEquals(containsYN, expected);
+    }
 
 }
